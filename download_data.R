ef1c7aa7032fdeb7c9b2913139f874ce6c844726
Kris Bruurs <krisbruurs@gmail.com>
2026-02-19 15:03:52 +0100

    finished modelling

---
+++ download_data.R
@@ -0,0 +1,5 @@
+# Download dataset
+system("kaggle datasets download -d theaimindset/customer-behavior-and-churn-simulation-dataset")
+
+# Unzip it
+unzip("customer-behavior-and-churn-simulation-dataset.zip", exdir = "data")
