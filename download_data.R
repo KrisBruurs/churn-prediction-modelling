c49be461b1da338783ea2798e61e7d00294f1b33
Kris Bruurs <krisbruurs@gmail.com>
2026-02-21 08:32:01 +0100

    updated download script;
    .quit

--- download_data.R
+++ download_data.R
@@ -1,3 +1,6 @@
+# Create data folder
+dir.create("data", showWarnings = FALSE)
+
 # Download dataset
 system('kaggle datasets download -d theaimindset/customer-behavior-and-churn-simulation-dataset')
 
@@ -6,3 +9,4 @@ unzip('customer-behavior-and-churn-simulation-dataset.zip', exdir = 'data')
 
 # Remove zipfile from repository
 file.remove('customer-behavior-and-churn-simulation-dataset.zip')
+
